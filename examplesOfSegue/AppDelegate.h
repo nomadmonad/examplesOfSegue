1f2b4b3e3958141f413ffcd7d181fe10a6dce306
nomadmonad <nomadmonad@gmail.com>
2014-10-13 14:49:22 +0900

    initial commit

---
+++ examplesOfSegue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  examplesOfSegue
+//
+//  Created by 山本 剛 on 2014/10/13.
+//  Copyright (c) 2014年 Tsuyoshi Yamamoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ examplesOfSegue/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  examplesOfSegue
+//
+//  Created by 山本 剛 on 2014/10/13.
+//  Copyright (c) 2014年 Tsuyoshi Yamamoto. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
